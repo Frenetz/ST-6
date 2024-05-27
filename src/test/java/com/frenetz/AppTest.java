c28666f2a70bfdfd9e3554e0c65bee9ac0e43b2f
Frenetz <xodyrev.fedya@mail.ru>
2024-05-27 17:25:15 +0300

    add some tests

--- src/test/java/com/frenetz/AppTest.java
+++ src/test/java/com/frenetz/AppTest.java
@@ -22,81 +22,78 @@ public class AppTest {
     }
 
     @Test
-    @DisplayName("Init test")
-    public void checkInitStates() {
-        assertEquals(game.state, State.PLAYING);
-        assertEquals(game.player1.symbol, 'X');
-        assertEquals(game.player2.symbol, 'O');
-        assertEquals(game.board.length, 9);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/game_data.csv")
-    @DisplayName("Check game states test")
-    public void checkGameStateTest(
-            String c1,
-            String c2,
-            String c3,
-            String c4,
-            String c5,
-            String c6,
-            String c7,
-            String c8,
-            String c9,
-            State expectedState) {
-        game.symbol = 'X';
-        char[] board = new char[]{
-                c1.charAt(0), c2.charAt(0), c3.charAt(0),
-                c4.charAt(0), c5.charAt(0), c6.charAt(0),
-                c7.charAt(0), c8.charAt(0), c9.charAt(0)
-        };
-        assertEquals(expectedState, game.checkState(board));
-    }
-
-    @Test
-    @DisplayName("Full moves test")
-    public void checkGenerateFullMoves() {
-        char[] board = {
-                ' ', ' ', ' ',
-                ' ', ' ', ' ',
-                ' ', ' ', ' '
-        };
-        ArrayList<Integer> result = new ArrayList<>();
-
-        game.generateMoves(board, result);
-
-        for (int i = 0; i < 9; i++) {
-            assertEquals(i, result.get(i));
+    @DisplayName("Initial Board State")
+    public void checkInitialBoardState() {
+        for (char cell : game.board) {
+            assertEquals(cell, ' ');
         }
     }
 
     @Test
-    @DisplayName("Zero moves test")
-    public void checkGenerateZeroMoves() {
-        char[] board = {
-                'X', 'O', 'X',
-                'O', 'X', 'O',
-                'O', 'X', 'O'
-        };
-        ArrayList<Integer> result = new ArrayList<>();
+    @DisplayName("Move and State Update Test")
+    public void checkMoveAndStateUpdate() {
+        game.board[0] = 'X';
+        game.state = game.checkState(game.board);
+        assertEquals(game.state, State.PLAYING);
 
-        game.generateMoves(board, result);
-
-        assertEquals(0, result.size());
+        game.board[1] = 'X';
+        game.board[2] = 'X';
+        game.state = game.checkState(game.board);
+        assertEquals(game.state, State.XWIN);
     }
 
     @Test
-    @DisplayName("Check evaluate position to inf")
-    public void checkEvaluatePositionInf() {
-        char[] board = {
-                'X', 'O', 'O',
-                ' ', 'X', 'O',
-                ' ', ' ', 'X'
-        };
+    @DisplayName("OWin State Test")
+    public void checkOWinState() {
+        game.board[0] = 'O';
+        game.board[1] = 'O';
+        game.board[2] = 'O';
+        game.state = game.checkState(game.board);
+        assertEquals(game.state, State.OWIN);
+    }
 
-        game.symbol = 'X';
+    @Test
+    @DisplayName("Draw State Test")
+    public void checkDrawState() {
+        game.board = new char[]{'X', 'O', 'X', 'X', 'O', 'O', 'O', 'X', 'X'};
+        game.state = game.checkState(game.board);
+        assertEquals(game.state, State.DRAW);
+    }
 
-        assertEquals(game.evaluatePosition(board, game.player1), Game.INF);
+    @Test
+    @DisplayName("Playing State Test")
+    public void checkPlayingState() {
+        game.board[0] = 'X';
+        game.board[1] = 'O';
+        game.board[2] = 'X';
+        game.board[3] = 'O';
+        game.board[4] = ' ';
+        game.board[5] = 'X';
+        game.board[6] = 'O';
+        game.board[7] = 'X';
+        game.board[8] = 'O';
+        game.state = game.checkState(game.board);
+        assertEquals(game.state, State.PLAYING);
+    }
+
+    @Test
+    @DisplayName("Generate Moves Test")
+    public void checkGenerateMoves() {
+        ArrayList<Integer> moveList = new ArrayList<>();
+        game.board[0] = 'X';
+        game.board[1] = 'O';
+        game.board[2] = 'X';
+        game.board[3] = ' ';
+        game.board[4] = 'O';
+        game.board[5] = ' ';
+        game.board[6] = 'O';
+        game.board[7] = 'X';
+        game.board[8] = ' ';
+        game.generateMoves(game.board, moveList);
+        assertEquals(moveList.size(), 3);
+        assertTrue(moveList.contains(3));
+        assertTrue(moveList.contains(5));
+        assertTrue(moveList.contains(8));
     }
 
     @Test
@@ -111,80 +108,37 @@ public class AppTest {
     }
 
     @Test
-    @DisplayName("Cell constructor")
-    public void checkCellInit() {
-        assertAll("Cell Constructor",
-                () -> assertDoesNotThrow(() -> new TicTacToeCell(1, 0, 2)),
-                () -> {
-                    TicTacToeCell cell = new TicTacToeCell(1, 0, 2);
-                    assertEquals(1, cell.getNum(), "Cell number should be 1");
-                    assertEquals(0, cell.getCol(), "Column index should be 0");
-                    assertEquals(2, cell.getRow(), "Row index should be 2");
-                    assertEquals(' ', cell.getMarker(), "Marker should be space character");
-                }
-        );
+    @DisplayName("Evaluate Position Test")
+    public void checkEvaluatePosition() {
+        game.board = new char[]{'X', 'X', 'X', 'O', 'O', ' ', ' ', ' ', ' '};
+        assertEquals(game.evaluatePosition(game.board, game.player1), Game.INF);
+        assertEquals(game.evaluatePosition(game.board, game.player2), -Game.INF);
+
+        game.board = new char[]{'O', 'O', 'O', 'X', 'X', ' ', ' ', ' ', ' '};
+        assertEquals(game.evaluatePosition(game.board, game.player1), -Game.INF);
+        assertEquals(game.evaluatePosition(game.board, game.player2), Game.INF);
+
+        game.board = new char[]{'X', 'O', 'X', 'X', 'O', 'O', 'O', 'X', 'X'};
+        assertEquals(game.evaluatePosition(game.board, game.player1), 0);
+        assertEquals(game.evaluatePosition(game.board, game.player2), 0);
+
+        game.board = new char[]{'X', 'O', ' ', ' ', 'X', 'O', ' ', ' ', ' '};
+        assertEquals(game.evaluatePosition(game.board, game.player1), -1);
+        assertEquals(game.evaluatePosition(game.board, game.player2), -1);
     }
 
     @Test
-    @DisplayName("Minimax win")
-    public void checkMinimaxWin() {
-        char[] board = {
-                ' ', ' ', 'X',
-                ' ', 'O', 'X',
-                ' ', ' ', ' '
-        };
-        int expectedMove = 9;
+    @DisplayName("Generate Moves on Empty and Full Board")
+    public void checkGenerateMovesOnEmptyAndFullBoard() {
+        game.board = new char[]{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        ArrayList<Integer> moveList = new ArrayList<>();
+        game.generateMoves(game.board, moveList);
+        assertEquals(moveList.size(), 9);
 
-        int actualMove = game.MiniMax(board, game.player2);
-
-        assertEquals(expectedMove, actualMove);
-    }
-
-    @Test
-    @DisplayName("Setting marker")
-    public void checkSettingMarker() {
-        TicTacToeCell cell = new TicTacToeCell(1, 0, 2);
-        cell.setMarker("TEST");
-
-        assertAll(
-                () -> assertEquals('T', cell.getMarker(), "Marker should be set to 'T'"),
-                () -> assertEquals("TEST", cell.getText(), "Text should be set to 'TEST'"),
-                () -> assertFalse(cell.isEnabled(), "Cell should be disabled after setting marker")
-        );
-    }
-
-    @Test
-    @DisplayName("Check evaluate position to zero")
-    public void checkEvaluatePositionZero() {
-        char[] board = {
-                'X', 'X', 'O',
-                'O', 'O', 'X',
-                'X', 'O', 'X'
-        };
-
-        assertEquals(game.evaluatePosition(board, game.player1), 0);
-    }
-
-    @Test
-    @DisplayName("Display method")
-    public void checkDisplayMethod() {
-        assertAll(
-                "Utility print",
-                () -> assertDoesNotThrow(() -> Utility.print(new char[]{'X', 'X', 'O', 'O', ' ', 'X', ' ', ' ', 'O'})),
-                () -> assertDoesNotThrow(() -> Utility.print(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8})),
-                () -> {
-                    ArrayList<Integer> test = new ArrayList<>();
-                    test.add(0);
-                    test.add(0);
-                    test.add(0);
-                    test.add(0);
-                    test.add(0);
-                    test.add(0);
-                    test.add(0);
-                    test.add(0);
-                    test.add(0);
-                    assertDoesNotThrow(() -> Utility.print(test));
-                });
+        game.board = new char[]{'X', 'O', 'X', 'X', 'O', 'O', 'O', 'X', 'X'};
+        moveList.clear();
+        game.generateMoves(game.board, moveList);
+        assertEquals(moveList.size(), 0);
     }
 
     @Test
